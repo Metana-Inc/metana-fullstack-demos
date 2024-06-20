774c4aee3e0dcaeab0b7bfffb6293b73b4e5772e
Sumuditha Lansakara <sumudithalanz@gmail.com>
2024-06-20 21:37:32 +0530

    fixed usersRouter

--- module-5/5.10/routes/usersRouter.js
+++ module-5/5.10/routes/usersRouter.js
@@ -1,75 +1,78 @@
 // Routes for the Blogs API
-import express from 'express';
+import express from "express";
 const usersRouter = express.Router();
 import {
+  users,
   getUsers,
   getUserById,
   addUser,
   deleteUser,
   updateUser,
   getUserByEmail,
-} from '../data/users.js';
+} from "../data/users.js";
 
 // All these routes will be scoped under /api/user in index.js
 
 // Get all users
-usersRouter.get('/', (req, res) => {
+usersRouter.get("/", (req, res) => {
   const data = getUsers();
   res.json(data);
 });
 
 // Get single user
-usersRouter.get('/:id', (req, res) => {
+usersRouter.get("/:id", (req, res) => {
   const { id } = req.params;
   const user = getUserById(id);
   if (!user) {
-    return res.status(404).json({ error: 'user not found' });
+    return res.status(404).json({ error: "user not found" });
   }
   res.status(200).json(user);
 });
 
 // Post new user
-usersRouter.post('/', (req, res) => {
+usersRouter.post("/", (req, res) => {
   try {
     const { name, email, role } = req.body;
     // Validate content
     if (!name || !email) {
-      throw new Error('required values: user or email are empty');
+      throw new Error("required values: user or email are empty");
     }
 
-    const user = addUser({ name, email, role });
     // Validate user doesn't exist already
     const existing = getUserByEmail(email);
     if (!!existing) {
-      throw new Error('user with email exists');
+      throw new Error("user with email exists");
     }
 
-    console.log('posted user: ', user);
-    res.json(user);
+    // Adding the user
+    const user = addUser({ name, email, role });
+
+    console.log("posted user: ", user);
   } catch (err) {
     return res.status(400).json({
       error: err.toString(),
     });
   }
+  res.json(users);
 });
 
 // Get single user
-usersRouter.get('/:id', (req, res) => {
+usersRouter.get("/:id", (req, res) => {
   const user = getUserById(req.params.id);
   if (!user) {
-    return res.status(404).json({ error: 'user not found' });
+    return res.status(404).json({ error: "user not found" });
   }
   res.status(200).json(user);
 });
 
 // Update single user
-usersRouter.put('/:id', (req, res) => {
+usersRouter.put("/:id", (req, res) => {
   try {
     const { id } = req.params;
     const { name, email, role } = req.body;
     // Validate content
     if (!id || !name || !email) {
-      throw new Error('required values: user or email are empty');
+      throw new Error("required values: user or email are empty");
     }
     const existing = getUserById(id);
     if (!existing) {
@@ -87,7 +90,7 @@ usersRouter.put('/:id', (req, res) => {
 });
 
 // Delete single user
-usersRouter.delete('/:id', (req, res) => {
+usersRouter.delete("/:id", (req, res) => {
   try {
     const { id } = req.params;
     const user = getUserById(id);
