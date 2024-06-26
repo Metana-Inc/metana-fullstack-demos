f6c01b448bed663b41761ce349cc4db5f3bacc55
Sumuditha Lansakara <sumudithalanz@gmail.com>
2024-06-26 15:23:19 +0530

    6.06 - Update endpoints to handle CRUD operations with PostgreSQL.

---
+++ module-6/6.06/db/dbconn.js
@@ -0,0 +1,20 @@
+import pg from 'pg';
+
+import { POSTGRES_URL } from '../config.js';
+
+const db = new pg.Pool({
+  connectionString: POSTGRES_URL,
+});
+
+// Test the connection
+db.connect((err, client, release) => {
+  console.log(`=== debug: starting PG connection...`);
+  if (err) {
+    console.error('Failed to connect to the database:', err);
+  } else {
+    console.log('Successfully connected to the database');
+    release();
+  }
+});
+
+export default db;
