2470177fb6958788926f9c1071872f9aeda0f4a7
Matt <mattfields+dev@pm.me>
2024-07-02 22:47:52 +1000

    add some placholder text

--- module-8/8.8/client/src/pages/About.js
+++ module-8/8.8/client/src/pages/About.js
@@ -2,7 +2,28 @@ function About() {
   return (
     <div>
       <h2>About</h2>
-      <p>This is the About page</p>
+      <p>About this site</p>
+      <p>
+        Praesent purus eros, fringilla a posuere vitae, imperdiet quis dui. Sed
+        quis consequat massa. Etiam nisi ligula, ultricies sed molestie id,
+        scelerisque quis eros. Donec laoreet, dolor sed euismod porttitor, neque
+        neque pharetra erat, id ultricies massa nisl ac lacus. Pellentesque ut
+        felis lectus, non sodales nibh. Cras eget lectus ipsum. Donec urna
+        dolor, elementum i.
+      </p>
+      <p>
+        Egestas eleifend, dapibus ac justo. Donec urna dolor, elementum in
+        egestas eleifend, dapibus ac justo. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Vestibulum dignissim fermentum diam, quis
+        imperdiet magna ultricies faucibus. In sagittis, nunc sit amet feugiat
+        auctor, tortor orci fermentum nibh.
+      </p>
+      <p>
+        In placerat urna mauris eget tortor. Aenean ac dolor velit. Aenean velit
+        nibh, condimentum id auctor quis, ultricies in turpis. Nunc euismod
+        ultrices viverra. Pellentesque ut felis lectus, non sodales nibh.
+        Quisque at augue quis tortor euismo.
+      </p>
     </div>
   );
 }
