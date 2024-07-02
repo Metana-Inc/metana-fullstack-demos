d05483cc57318d06a2afbdf7541cf9a04722a526
Matt <mattfields+dev@pm.me>
2024-07-02 18:07:37 +1000

    cleanup debugs

--- module-8/8.6/server/db/dbconn.js
+++ module-8/8.6/server/db/dbconn.js
@@ -4,7 +4,6 @@ import { MONGO_URI } from '../config.js';
 export async function connectToDatabase() {
   try {
     console.log('connecting to mongo database...');
-    console.log(`=== debug: MONGO_URI: ${MONGO_URI}`);
     await mongoose.connect(MONGO_URI);
     console.log('MongoDB connected');
   } catch (err) {
