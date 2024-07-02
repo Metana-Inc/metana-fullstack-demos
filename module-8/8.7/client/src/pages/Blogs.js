96fefce7fc4ed90dd36d1ce5e454ef766c48dcb7
Matt <mattfields+dev@pm.me>
2024-07-02 18:43:21 +1000

    fix api proxy

--- module-8/8.7/client/src/pages/Blogs.js
+++ module-8/8.7/client/src/pages/Blogs.js
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function Blogs() {
+  const [blogs, setBlogs] = useState();
+
+  useEffect(() => {
+    console.log('=== debug: fetching blogs data...');
+    async function getBlogs() {
+      const data = await axios.get('/api/blogs');
+      console.log('=== debug: data returned:', data);
+    }
+    getBlogs();
+  }, []);
+
   return (
     <div>
       <h2>Blogs</h2>
