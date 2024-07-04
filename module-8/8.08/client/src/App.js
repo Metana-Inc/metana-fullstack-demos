6c2c0cf9becfb1dcdae6cdc656253e74ba29bbea
Matt <394065+fieldse@users.noreply.github.com>
2024-07-04 18:56:14 +1000

    Merge module 8 (#14)
    Add complete working React setup with TailwindCSS

---
+++ module-8/8.08/client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './layouts/Layout';
+import Home from './pages/Home';
+import Blogs from './pages/Blogs';
+import BlogItem from './pages/BlogItem';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/blogs/:id" element={<BlogItem />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ module-8/8.09/client/src/pages/Contact.js
@@ -0,0 +1,44 @@
+import './Contact.css';
+function Contact() {
+  const email = 'contact@example.com';
+  const facebook = '#';
+  const twitterX = '#';
+  return (
+    <div>
+      <h2>Contact</h2>
+
+      <div>
+        <p>
+          Condimentum id auctor quis, ultricies in turpis. Vestibulum suscipit
+          commodo ante quis tincidunt. Cras posuere fringilla lectus, ac posuere
+          ligula auctor non. Mauris metus nulla, aliquet non aliquam a, lacinia
+          sed nulla. Sed quis consequat massa. In hac habitasse plate.
+        </p>
+      </div>
+      <div className="margin-top-30">
+        <h3>Get in touch</h3>
+        <p>If you'd like to get in touch, I'd love to hear from you.</p>
+
+        <div className="contact-methods">
+          <p>
+            <span className="contact-method-item">
+              <a href={`mailto:${email}`}>Email</a>
+            </span>
+          </p>
+          <p>
+            <span className="contact-method-item">
+              <a href={facebook}>Facebook</a>
+            </span>
+          </p>
+          <p>
+            <span className="contact-method-item">
+              <a href={twitterX}>Twitter / X</a>
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
