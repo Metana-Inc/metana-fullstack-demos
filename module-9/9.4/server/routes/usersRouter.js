4553a48a9a9c8c4933f20b99df614583d233b3c7
Matt <mattfields+dev@pm.me>
2024-07-13 14:42:59 +1000

    add password hashing on user routes

--- module-9/9.4/server/routes/usersRouter.js
+++ module-9/9.4/server/routes/usersRouter.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import User from '../models/User.js';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const usersRouter = express.Router();
 
@@ -35,7 +36,12 @@ usersRouter.post('/', async (req, res) => {
     if (!name || !email || !name || !password) {
       throw new Error('user values cannot be empty');
     }
-    const newUser = new User({ name, email, role, password });
+
+    // hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Store new user with hashed password
+    const newUser = new User({ name, email, role, hashedPassword });
     await newUser.save();
     res.status(201).json(newUser);
   } catch (error) {
@@ -51,9 +57,24 @@ usersRouter.put('/:id', async (req, res) => {
     if (!name || !email || !name || !password) {
       throw new Error('user values cannot be empty');
     }
+
+    // Todo: validate current user has rights to modify user roles
+    if (role) {
+      const prevUser = User.findOne({ email: email });
+      if (prevUser && prevUser.role !== role) {
+        console.log(
+          '[warning] -- user role updated. FIXME: should be admin-only operation'
+        );
+      }
+    }
+
+    // hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Update user
     const updatedUser = await User.findByIdAndUpdate(
       id,
-      { name, email, role, password },
+      { name, email, role, password: hashedPassword },
       { new: true }
     );
     if (!updatedUser) {
