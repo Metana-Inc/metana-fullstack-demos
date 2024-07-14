017006774c1acb84f8de4984f5e25deaeec9b720
Matt <mattfields+dev@pm.me>
2024-07-14 13:45:54 +1000

    rename JWT_SECRET

--- module-9/9.5/server/config.js
+++ module-9/9.5/server/config.js
@@ -4,4 +4,4 @@ import 'dotenv/config';
 export const PORT = process.env.PORT || 3001;
 export const MONGO_URI = process.env.MONGO_URI;
 
-export const SERVER_ENCRYPT_KEY = process.env.SERVER_ENCRYPT_KEY;
+export const JWT_SECRET = process.env.JWT_SECRET;
