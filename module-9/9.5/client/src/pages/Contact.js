a5f703abadc48fc2f7139788d1191b5f985be626
Matt <mattfields+dev@pm.me>
2024-07-16 14:41:27 +1000

    change "class=" -> className

--- module-9/9.5/client/src/pages/Contact.js
+++ module-9/9.5/client/src/pages/Contact.js
@@ -26,25 +26,25 @@ function ContactForm() {
       <form
         action="#"
         method="POST"
-        class="mx-auto mt-16 max-w-xl sm:mt-20"
+        className="mx-auto mt-16 max-w-xl sm:mt-20"
         onSubmit={handleSubmit}
       >
-        <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           {/* Firstname */}
           <div>
             <label
               for="first-name"
-              class="block text-sm font-semibold leading-6 text-gray-900"
+              className="block text-sm font-semibold leading-6 text-gray-900"
             >
               First name
             </label>
-            <div class="mt-2.5">
+            <div className="mt-2.5">
               <input
                 type="text"
                 name="first-name"
                 id="first-name"
                 autocomplete="given-name"
-                class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 value={firstName}
                 onChange={(e) => setFirstName(e.target.value)}
               />
@@ -55,17 +55,17 @@ function ContactForm() {
           <div>
             <label
               for="last-name"
-              class="block text-sm font-semibold leading-6 text-gray-900"
+              className="block text-sm font-semibold leading-6 text-gray-900"
             >
               Last name
             </label>
-            <div class="mt-2.5">
+            <div className="mt-2.5">
               <input
                 type="text"
                 name="last-name"
                 id="last-name"
                 autocomplete="family-name"
-                class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 value={lastName}
                 onChange={(e) => setLastName(e.target.value)}
               />
@@ -73,20 +73,20 @@ function ContactForm() {
           </div>
 
           {/* Email */}
-          <div class="sm:col-span-2">
+          <div className="sm:col-span-2">
             <label
               for="email"
-              class="block text-sm font-semibold leading-6 text-gray-900"
+              className="block text-sm font-semibold leading-6 text-gray-900"
             >
               Email
             </label>
-            <div class="mt-2.5">
+            <div className="mt-2.5">
               <input
                 type="email"
                 name="email"
                 id="email"
                 autocomplete="email"
-                class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
@@ -94,19 +94,19 @@ function ContactForm() {
           </div>
 
           {/* Comments */}
-          <div class="sm:col-span-2">
+          <div className="sm:col-span-2">
             <label
               for="comments"
-              class="block text-sm font-semibold leading-6 text-gray-900"
+              className="block text-sm font-semibold leading-6 text-gray-900"
             >
               Comments
             </label>
-            <div class="mt-2.5">
+            <div className="mt-2.5">
               <input
                 type="text"
                 name="comments"
                 id="comments"
-                class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 value={comments}
                 placeholder="Leave a comment"
                 onChange={(e) => setComments(e.target.value)}
@@ -114,10 +114,10 @@ function ContactForm() {
             </div>
           </div>
         </div>
-        <div class="mt-10">
+        <div className="mt-10">
           <button
             type="submit"
-            class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Leave me a message
           </button>
--- module-9/9.5/client/src/pages/Login.js
+++ module-9/9.5/client/src/pages/Login.js
@@ -18,29 +18,29 @@ function LoginPage() {
           action="#"
           className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/3 min-w-72"
         >
-          <div class="mb-4">
+          <div className="mb-4">
             <label
-              class="block text-gray-700 text-sm font-bold mb-2"
+              className="block text-gray-700 text-sm font-bold mb-2"
               for="username"
             >
               Username
             </label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="username"
               type="text"
               placeholder="Username"
             />
           </div>
-          <div class="mb-6">
+          <div className="mb-6">
             <label
-              class="block text-gray-700 text-sm font-bold mb-2"
+              className="block text-gray-700 text-sm font-bold mb-2"
               for="password"
             >
               Password
             </label>
             <input
-              class={`shadow appearance-none border ${
+              className={`shadow appearance-none border ${
                 hasError ? 'border-red-600' : ''
               } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
               id="password"
@@ -48,21 +48,21 @@ function LoginPage() {
               placeholder="******************"
             />
             {hasError && (
-              <p class="text-red-500 text-xs italic hidden">
+              <p className="text-red-500 text-xs italic hidden">
                 Please choose a password.
               </p>
             )}
           </div>
-          <div class="flex items-center justify-between">
+          <div className="flex items-center justify-between">
             <button
-              class="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
               onClick={loginAction}
             >
               Sign In
             </button>
             <a
-              class="inline-block align-baseline font-bold text-sm text-green-500 hover:text-green-600"
+              className="inline-block align-baseline font-bold text-sm text-green-500 hover:text-green-600"
               href={forgotPasswordUrl}
             >
               Forgot Password?
