6c1031afc8ee7e10f089add9b29738325dafba2c
Matt <mattfields+dev@pm.me>
2024-07-16 15:07:45 +1000

    get frontend correctly posting to API backend

--- module-9/9.6/client/src/pages/Login.js
+++ module-9/9.6/client/src/pages/Login.js
@@ -1,76 +1,142 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { login, logout } from '../auth/AuthProvider';
 
 // The login view
 function LoginPage() {
-  const [hasError] = useState(false);
-  const loginAction = () =>
-    console.log('=== debug: placeholder for login action');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const forgotPasswordUrl = '#';
+  const logoutAction = async () => {
+    try {
+      const data = await logout();
+      console.log('logged out');
+      console.log('=== debug: user: ', data);
+      setIsLoggedIn(false);
+    } catch (err) {
+      console.log('error', err);
+    }
+  };
 
   return (
     <div id="wrapper" className="grid h-screen place-items-center">
       <div className="relative -top-20">
         <h2 className="text-2xl text-center mb-8 relative">Login</h2>
-        <form
-          id="login"
-          method="POST"
-          action="#"
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/3 min-w-72"
-        >
-          <div class="mb-4">
-            <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="username"
-            >
-              Username
-            </label>
-            <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              placeholder="Username"
-            />
-          </div>
-          <div class="mb-6">
-            <label
-              class="block text-gray-700 text-sm font-bold mb-2"
-              for="password"
-            >
-              Password
-            </label>
-            <input
-              class={`shadow appearance-none border ${
-                hasError ? 'border-red-600' : ''
-              } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
-              id="password"
-              type="password"
-              placeholder="******************"
-            />
-            {hasError && (
-              <p class="text-red-500 text-xs italic hidden">
-                Please choose a password.
-              </p>
-            )}
-          </div>
-          <div class="flex items-center justify-between">
-            <button
-              class="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-              onClick={loginAction}
-            >
-              Sign In
-            </button>
-            <a
-              class="inline-block align-baseline font-bold text-sm text-green-500 hover:text-green-600"
-              href={forgotPasswordUrl}
-            >
-              Forgot Password?
-            </a>
-          </div>
-        </form>
+        <h1 className="text-2xl text-red-600 my-6">
+          {isLoggedIn ? 'You are currently logged in' : 'You are not logged in'}
+        </h1>
       </div>
+      {isLoggedIn ? (
+        <div className="my-20">
+          <LogoutButton onClick={logoutAction} />
+        </div>
+      ) : (
+        <LoginForm setIsLoggedIn={setIsLoggedIn} />
+      )}
     </div>
+  );
+}
+const LogoutButton = ({ onClick }) => (
+  <button
+    className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+    type="button"
+    onClick={onClick}
+  >
+    Sign out
+  </button>
+);
+
+// The login form
+function LoginForm({ setIsLoggedIn }) {
+  const [hasError] = useState(false);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [canSubmit, setCanSubmit] = useState(false);
+  const forgotPasswordUrl = '#';
+
+  const loginAction = async () => {
+    try {
+      console.log('starting login...');
+      console.log('=== debug: email: ', email, ' password: ', password);
+      const data = await login({ email, password });
+      console.log('=== debug: user: ', data);
+      setIsLoggedIn(true);
+      console.log('logged in');
+    } catch (err) {
+      console.log('error', err);
+    }
+  };
+
+  // Enable the submit button on all fields filled
+  useEffect(() => {
+    if (email?.length && password?.length) {
+      setCanSubmit(true);
+    } else {
+      setCanSubmit(false);
+    }
+  }, [email, password]);
+
+  return (
+    <form
+      id="login"
+      method="POST"
+      action="#"
+      className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/3 min-w-72"
+    >
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="username"
+        >
+          Username
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="username"
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          className={`shadow appearance-none border ${
+            hasError ? 'border-red-600' : ''
+          } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+          id="password"
+          type="password"
+          placeholder="******************"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {hasError && (
+          <p className="text-red-500 text-xs italic hidden">
+            Please choose a password.
+          </p>
+        )}
+      </div>
+      <div className="flex items-center justify-between">
+        <button
+          className={`${
+            canSubmit ? 'bg-green-500 hover:bg-green-600' : 'bg-gray-300'
+          } text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+          type="button"
+          onClick={loginAction}
+          disabled={!canSubmit}
+        >
+          Sign In
+        </button>
+        <a
+          className="inline-block align-baseline font-bold text-sm text-green-500 hover:text-green-600"
+          href={forgotPasswordUrl}
+        >
+          Forgot Password?
+        </a>
+      </div>
+    </form>
   );
 }
 export default LoginPage;
