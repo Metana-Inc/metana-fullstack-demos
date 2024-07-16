14b65a993aeecf14cc5575c6ea8811635a225946
Matt <mattfields+dev@pm.me>
2024-07-16 16:28:33 +1000

    9.7 - add dir

---
+++ module-9/9.7/client/src/auth/AuthProvider.js
@@ -0,0 +1,40 @@
+// Context provider for login/logout and authentication routes.
+
+import authAPI from '../api/authAPI';
+
+// Method that does the actual login, and stores the response data to localStorage
+export async function login({ email, password }) {
+  console.log('=== debug: POST to /api/login ...');
+  const response = await authAPI.login({ email, password });
+  console.log(`=== debug: API response:`, response);
+  if (!response.success) {
+    throw new Error('login failed:', response?.message || 'unknown error');
+  }
+  const { user, token } = response.data;
+  if (!user || !token) {
+    throw new Error('response user or token is empty');
+  }
+  console.log(`=== debug: user:`, user);
+  console.log(`=== debug: token:`, token);
+
+  // Do something with the token
+  console.log('login successful');
+  saveAuthToken(token);
+  return user;
+}
+
+export async function logout() {
+  try {
+    clearAuthToken();
+  } catch (err) {
+    console.error('Logout error:', err);
+  }
+}
+
+const saveAuthToken = (token) => {
+  console.log('FIXME: Save Auth Token');
+};
+
+const clearAuthToken = () => {
+  console.log('FIXME: Clear Auth Token');
+};
---
+++ module-9/9.7/client/src/pages/Login.js
@@ -0,0 +1,149 @@
+import { useEffect, useState } from 'react';
+import { login, logout } from '../auth/AuthProvider';
+
+// The login view
+function LoginPage() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const logoutAction = async () => {
+    try {
+      const data = await logout();
+      console.log('logged out');
+      console.log('=== debug: user: ', data);
+      setIsLoggedIn(false);
+    } catch (err) {
+      console.log('error', err);
+    }
+  };
+
+  return (
+    <div id="wrapper" className="grid h-screen place-items-center">
+      <div className="relative -top-20">
+        <h2 className="text-2xl text-center mb-8 relative">Login</h2>
+        <h1 className="text-2xl text-red-600 my-6">
+          {isLoggedIn ? 'You are currently logged in' : 'You are not logged in'}
+        </h1>
+      </div>
+      {isLoggedIn ? (
+        <div className="my-20">
+          <LogoutButton onClick={logoutAction} />
+        </div>
+      ) : (
+        <LoginForm setIsLoggedIn={setIsLoggedIn} />
+      )}
+    </div>
+  );
+}
+const LogoutButton = ({ onClick }) => (
+  <button
+    className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+    type="button"
+    onClick={onClick}
+  >
+    Sign out
+  </button>
+);
+
+// The login form
+function LoginForm({ setIsLoggedIn }) {
+  const [hasError, setHasError] = useState(false);
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [canSubmit, setCanSubmit] = useState(false);
+  const forgotPasswordUrl = '#';
+
+  const loginAction = async () => {
+    try {
+      console.log('starting login...');
+      const user = await login({ email, password });
+      // Do something with the user here...
+      console.log('user: ', user);
+      setIsLoggedIn(true);
+      setHasError(false);
+      console.log('logged in');
+    } catch (err) {
+      console.log('login failed: ' + err.message);
+      setHasError(true);
+    }
+  };
+
+  // Enable the submit button on all fields filled
+  useEffect(() => {
+    if (email?.length && password?.length) {
+      setCanSubmit(true);
+    } else {
+      setCanSubmit(false);
+    }
+  }, [email, password]);
+
+  return (
+    <form
+      id="login"
+      method="POST"
+      action="#"
+      className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/3 min-w-72"
+    >
+      {hasError && (
+        <div className="h-12 warn text-red-500 text-center text-xl">
+          There was an error with your login.
+        </div>
+      )}
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="username"
+        >
+          Username
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="username"
+          type="text"
+          placeholder="Username"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="password"
+        >
+          Password
+        </label>
+        <input
+          className={`shadow appearance-none border ${
+            hasError ? 'border-red-600' : ''
+          } rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline`}
+          id="password"
+          type="password"
+          placeholder="******************"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {hasError && (
+          <p className="text-red-500 text-xs italic hidden">
+            Please choose a password.
+          </p>
+        )}
+      </div>
+      <div className="flex items-center justify-between">
+        <button
+          className={`${
+            canSubmit ? 'bg-green-500 hover:bg-green-600' : 'bg-gray-300'
+          } text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+          type="button"
+          onClick={loginAction}
+          disabled={!canSubmit}
+        >
+          Sign In
+        </button>
+        <a
+          className="inline-block align-baseline font-bold text-sm text-green-500 hover:text-green-600"
+          href={forgotPasswordUrl}
+        >
+          Forgot Password?
+        </a>
+      </div>
+    </form>
+  );
+}
+export default LoginPage;
