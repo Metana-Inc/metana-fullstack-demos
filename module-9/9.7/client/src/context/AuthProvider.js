b45ec8052c589d066708ff4ea891303da5e20300
Matt <mattfields+dev@pm.me>
2024-07-17 18:29:01 +1000

    create useAuth hook in AuthContext

--- module-9/9.7/client/src/context/AuthProvider.js
+++ module-9/9.7/client/src/context/AuthProvider.js
@@ -1,15 +1,26 @@
 // Provider for Auth context
-import { createContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 
-const AuthContext = createContext({});
+// Define our AuthContext
+const AuthContext = createContext({
+  user: undefined,
+  login: async () => {},
+  logout: async () => {},
+});
 
+// Define our AuthContext Provider. This can be used to wrap the app to provide access to the AuthContext.
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({});
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+  const [token, setToken] = useState({});
+  async function login() {}
+  async function logout() {}
   return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider value={{ isLoggedIn, user, token, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
 };
 
-export default AuthContext;
+// Create a hook we can use to get access to the AuthContext in our components.
+export const useAuth = () => useContext(AuthContext);
