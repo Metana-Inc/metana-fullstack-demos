4d1e7f968e2a59dfbdd6e1ae64a9be7d09d23ee1
Matt <mattfields+dev@pm.me>
2024-07-18 16:49:02 +1000

    fix protectedRoute logic

--- module-9/9.8/client/src/components/ProtectedRoute.js
+++ module-9/9.8/client/src/components/ProtectedRoute.js
@@ -1,10 +1,10 @@
-import { useAuth } from '../context';
+import { useAuth } from '../context/AuthProvider';
 import { Navigate } from 'react-router-dom';
 
 // ProtectedRoute is a wrapper for pages that require login
 const ProtectedRoute = ({ children }) => {
-  const { user } = useAuth();
-  if (!user) {
+  const { user, isLoggedIn } = useAuth();
+  if (!user || !isLoggedIn) {
     return <Navigate to="/login" replace />;
   }
   return children;
--- module-9/9.8/client/src/pages/Profile.js
+++ module-9/9.8/client/src/pages/Profile.js
@@ -1,7 +1,11 @@
 // User profile page
+
+import { LogoutButton } from './Login';
+import { useAuth } from '../context/AuthProvider';
+
 function ProfileRow({ label, value }) {
   return (
-    <div className="flex flex-row justify-between items-center">
+    <div className="flex flex-row justify-between items-center my-2">
       <label className="w-1/3">{label}</label>
       <input
         className="bg-white rounded-md flex-grow p-2 text-right"
@@ -14,6 +18,7 @@ function ProfileRow({ label, value }) {
 }
 
 function Profile() {
+  const { logout } = useAuth();
   return (
     <div
       id="profile-container"
@@ -21,15 +26,18 @@ function Profile() {
     >
       <div className="my-12">
         <h1 className="text-2xl mb-8 font-semibold">Your Profile</h1>
-        <p className="p-4 italic text-gray-600">
-          * This is a private route. You should only be able to view it if
-          logged in.
+        <p className="py-4 italic text-gray-600">
+          This is a private route. You should only be able to view it if logged
+          in.
         </p>
       </div>
       <div className="drop-shadow-xl border h-80 w-full bg-gray-200 py-10 px-10 border-gray-400 rounded-xl text-lg flex flex-col justify-around">
         <ProfileRow label="Name" value={'Example name'} />
         <ProfileRow label="Email" value={'Example Email'} />
         <ProfileRow label="Role" value={'Example Role'} />
+        <span className="my-8 text-center">
+          <LogoutButton onClick={logout} />
+        </span>
       </div>
     </div>
   );
