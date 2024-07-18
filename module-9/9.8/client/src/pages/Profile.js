eb4d6dba9cb42ac3e91a9e4c163970bac6d862b8
Matt <mattfields+dev@pm.me>
2024-07-18 21:46:08 +1000

    fix profile form data and styles

--- module-9/9.8/client/src/pages/Profile.js
+++ module-9/9.8/client/src/pages/Profile.js
@@ -6,9 +6,9 @@ import { useAuth } from '../context/AuthProvider';
 function ProfileRow({ label, value }) {
   return (
     <div className="flex flex-row justify-between items-center my-2">
-      <label className="w-1/3">{label}</label>
+      <label className="w-1/3 font-semibold">{label}</label>
       <input
-        className="bg-white rounded-md flex-grow p-2 text-right"
+        className="bg-white rounded-md flex-grow p-2 text-right text-gray-600"
         type="text"
         disabled
         value={value}
@@ -18,11 +18,12 @@ function ProfileRow({ label, value }) {
 }
 
 function Profile() {
-  const { logout } = useAuth();
+  const { user, logout } = useAuth();
+  console.log('=== debug: user: ', user);
   return (
     <div
       id="profile-container"
-      className="flex flex-col h-max justify-start items-center w-1/3 min-w-80 mx-auto"
+      className="flex flex-col h-max justify-start items-center lg:w-1/2 md:w-1/2 sm:w-3/4 min-w-xl max-w-2xl  mx-auto"
     >
       <div className="my-12">
         <h1 className="text-2xl mb-8 font-semibold">Your Profile</h1>
@@ -32,9 +33,9 @@ function Profile() {
         </p>
       </div>
       <div className="drop-shadow-xl border h-80 w-full bg-gray-200 py-10 px-10 border-gray-400 rounded-xl text-lg flex flex-col justify-around">
-        <ProfileRow label="Name" value={'Example name'} />
-        <ProfileRow label="Email" value={'Example Email'} />
-        <ProfileRow label="Role" value={'Example Role'} />
+        <ProfileRow label="Name" value={user.name} />
+        <ProfileRow label="Email" value={user.email} />
+        <ProfileRow label="Role" value={user.role} />
         <span className="my-8 text-center">
           <LogoutButton onClick={logout} />
         </span>
